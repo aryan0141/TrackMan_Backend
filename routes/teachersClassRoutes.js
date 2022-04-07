dd40fda6ea2c9a0b15d27db3f7d7a085e60187ee
Aryan Garg <garg2001aryan@gmail.com>
2022-04-07 12:26:34 +0530

    Updated error mailing

--- routes/teachersClassRoutes.js
+++ routes/teachersClassRoutes.js
@@ -22,43 +22,14 @@ const oauth2Client = new google.auth.OAuth2(
 
 Router.get("/courseList/:email", async (req, resp, next) => {
    try {
-    // console.log(req.params.email);
     const email9 = req.params.email;
-    // console.log(email9);
     var currentUser = await User.findOne({ email: email9 });
     var RefreshToken = currentUser.refreshToken;
-    // console.log(RefreshToken);
-
-  // User.findOne({email: email1} , function(err , user3){
-  //   console.log(user3);
-  // })
-
-  // const accToekn = req.params.ac_token;
-
     oauth2Client.setCredentials({ refresh_token: RefreshToken });
     const res = await classroom.courses.list({
       teacherId: "me",
     })
-    // .catch(e => {
-    //   console.log("error occurred");
-    //   console.error(e);
-    //   throw e;
-    // });
-    
-    console.log(res);
-    //   const auth = new google.auth.GoogleAuth({
-    //   scopes: [
-    //       "https://www.googleapis.com/auth/classroom.courses",
-    //       "https://www.googleapis.com/auth/classroom.courses.readonly",
-    //     ],
-    //   });
-
-    // const authClient = await auth.getClient(req.params.accessToken);
-    // google.options({ auth: authClient });
-
-  
     resp.json(res);
-    // resp.send(res.data);
   } catch (error) {
     mail.mailfunc("Error in /courseList/:email", error.toString());
     next(error);
